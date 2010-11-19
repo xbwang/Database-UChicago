d78829c7b663c1484031e7a1b1ee654f53b07b45
xbwang <vincent.wangxb@gmail.com>
2010-11-18 18:05:12 -0600

    Creation Modified, Constraints Added.

--- create_db.sql
+++ create_db.sql
@@ -1,84 +1,86 @@
-DROP TABLE IF EXISTS User;
-CREATE TABLE User(
-	user_id BIGINT UNSIGNED,
-	user_name VARCHAR(20) UNIQUE,
-	nick_name VARCHAR(20),
-	bio_info VARCHAR(200),
-	location_id INT UNSIGNED,
-	blog_link VARCHAR(100),
-	password VARCHAR(50),
-	/*email VARCHAR(50),*/
-	PRIMARY KEY (user_id)
-	)InnoDB;
-
-/*
-DROP TABLE IF EXISTS Profile;
-CREATE TABLE Profile(
-	profile_id BIGINT UNSIGNED,
-	user_id BIGINT UNSIGNED,
-	nick_name VARCHAR(20),
-	location_id INT UNSIGNED,
-	page_link VARCHAR(100),
-	photo_link VARCHAR(100),
-	blog_link VARCHAR(100),
-	bio_info VARCHAR(200),
-	PRIMARY KEY (profile_id)
-	);
-*/
-	
-DROP TABLE IF EXISTS Tweet;
-CREATE TABLE Tweet(
-	tweet_id BIGINT UNSIGNED,
-	poster_id BIGINT UNSIGNED,
-	time TIMESTAMP,
-	content VARCHAR(200),
-	location_id INT UNSIGNED,
-	PRIMARY KEY (tweet_id)
-	);
-
 DROP TABLE IF EXISTS FollowerList;
-CREATE TABLE FollowerList(
-	follower_id BIGINT UNSIGNED, /*first follows second*/
-	following_id BIGINT UNSIGNED,
-	/*follow_time DATETIME,*/
-	following_group VARCHAR(50),
-	PRIMARY KEY (follower_id, following_id)
-	);
-
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Tweet;
+DROP TABLE IF EXISTS Tag;
+DROP TABLE IF EXISTS ObtainAch;
+DROP TABLE IF EXISTS Achievement;
+DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Location;
+
 CREATE TABLE Location(
 	location_id INT UNSIGNED,
 	country VARCHAR(50),
 	state VARCHAR(50),
     PRIMARY KEY (location_id)
-	);
+	) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS Comment;
-CREATE TABLE Comment(
+CREATE TABLE User(
+	user_id BIGINT UNSIGNED,
+	user_name VARCHAR(20) UNIQUE,
+	nick_name VARCHAR(20),
+	bio_info VARCHAR(200),
+	location_id INT UNSIGNED REFERENCES Location(location_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	blog_link VARCHAR(100),
+	password VARCHAR(50),
+	PRIMARY KEY (user_id)
+	) ENGINE = InnoDB;
+
+CREATE TABLE Tweet(
 	tweet_id BIGINT UNSIGNED,
-	replyer_id BIGINT UNSIGNED,
+	poster_id BIGINT UNSIGNED REFERENCES User(user_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	time TIMESTAMP,
+	content VARCHAR(200),
+	location_id INT UNSIGNED REFERENCES Location(location_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	PRIMARY KEY (tweet_id)
+	) ENGINE = InnoDB;
+
+CREATE TABLE FollowerList(
+	follower_id BIGINT UNSIGNED REFERENCES User(user_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE, /*first follows second*/
+	following_id BIGINT UNSIGNED REFERENCES User(user_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	following_group VARCHAR(50),
+	PRIMARY KEY (follower_id, following_id)
+	) ENGINE = InnoDB;
+
+CREATE TABLE Comment(
+	tweet_id BIGINT UNSIGNED REFERENCES Tweet(tweet_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	replyer_id BIGINT UNSIGNED REFERENCES User(user_id),
 	content VARCHAR(200),
 	time TIMESTAMP,
 	PRIMARY KEY (tweet_id, replyer_id, time)
-	);
-	
-DROP TABLE IF EXISTS Tag;
+	) ENGINE = InnoDB;
+
 CREATE TABLE Tag(
-	user_id BIGINT UNSIGNED,
+	user_id BIGINT UNSIGNED REFERENCES User(user_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
 	tag_name VARCHAR(50),
 	PRIMARY KEY (user_id, tag_name)
-	);
+	) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS Achievement;
 CREATE TABLE Achievement(
 	achi_id INT UNSIGNED,
-	achi_name VARCHAR(50),
+	achi_name VARCHAR(50) UNIQUE,
 	PRIMARY KEY (achi_id)
-	);
+	) ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS ObtainAch;
 CREATE TABLE ObtainAch(
-	user_id BIGINT UNSIGNED,
-	achi_id INT UNSIGNED,
+	user_id BIGINT UNSIGNED REFERENCES User(user_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
+	achi_id INT UNSIGNED REFERENCES Achievement(achi_id)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE,
 	PRIMARY KEY (user_id, achi_id)
-	);
+	) ENGINE = InnoDB;
