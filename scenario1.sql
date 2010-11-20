db4636d39c548f63c41c65e3fec243c7c09f7f5c
xbwang <vincent.wangxb@gmail.com>
2010-11-19 21:18:17 -0600

    final version

---
+++ scenario1.sql
@@ -0,0 +1,70 @@
+/*achievement trigger test*/
+SELECT '*************************************';
+DELETE FROM Achievement WHERE achi_name = 'achievement test1';
+DELETE FROM Achievement WHERE achi_name = 'achievement test2';
+DROP FUNCTION IF EXISTS FindMax;
+DELIMITER |
+CREATE FUNCTION FindMax()
+RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE max INT;
+	SET max = (SELECT MAX(achi_id) FROM Achievement);
+	RETURN max;
+END |
+DELIMITER ;
+/*1st insert statement will active the trigger*/
+INSERT INTO Achievement VALUES(1000, 'achievement test1');
+/*show the trigger works*/
+SELECT * FROM Achievement WHERE achi_name = 'achievement test1';
+/*2st insert statement will not active the trigger*/
+SELECT FindMax(); /*show the max value*/
+INSERT INTO Achievement VALUES(FindMax()+1, 'achievement test2');
+/*show the trigger not work*/
+SELECT * FROM Achievement WHERE achi_name = 'achievement test2';
+
+/*user trigger test*/
+SELECT '*************************************';
+DELETE FROM User WHERE user_name = 'user test1';
+DELETE FROM User WHERE user_name = 'user test2';
+DROP FUNCTION IF EXISTS FindMax;
+DELIMITER |
+CREATE FUNCTION FindMax()
+RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE max INT;
+	SET max = (SELECT MAX(user_id) FROM User);
+	RETURN max;
+END |
+DELIMITER ;
+/*1st insert statement will active the trigger*/
+INSERT INTO User VALUES(1, 'user test1', 'test', 'test', 70, 'test', 'test');
+/*show the trigger works*/
+SELECT user_id, user_name, location_id FROM User WHERE user_name = 'user test1';
+/*2st insert statement will not active the trigger*/
+SELECT FindMax(); /*show the max value*/
+INSERT INTO User VALUES(FindMax()+1, 'user test2', 'test', 'test', 11, 'test', 'test');
+/*show the trigger not work*/
+SELECT user_id, user_name, location_id FROM User WHERE user_name = 'user test2';
+
+/*tweet trigger test*/
+SELECT '*************************************';
+DELETE FROM Tweet WHERE content = 'user test2';
+DROP FUNCTION IF EXISTS FindMax;
+DELIMITER |
+CREATE FUNCTION FindMax()
+RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE max INT;
+	SET max = (SELECT MAX(tweet_id) FROM Tweet);
+	RETURN max;
+END |
+DELIMITER ;
+/*1st insert statement will active the trigger*/
+INSERT INTO Tweet VALUES(1, 14431558, 20201119010101, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 70);
+/*show the trigger works*/
+SELECT tweet_id, poster_id, content, location_id FROM Tweet WHERE time = 20201119010101;
+/*2nd insert statement will not active the trigger*/
+SELECT FindMax();
+INSERT INTO Tweet VALUES(FindMax()+1, 14431558, 20201119010102, 'user test2', 11);
+/*show the trigger not work*/
+SELECT tweet_id, poster_id, content, location_id FROM Tweet WHERE time = 20201119010102;
--- triggers.sql
+++ triggers.sql
@@ -5,8 +5,8 @@ CREATE TRIGGER AchievementTrigger
 BEFORE INSERT ON Achievement
 FOR EACH ROW
 BEGIN
-	IF NEW.achi_id != (SELECT COUNT(*) FROM Achievement)+1 THEN
-		SET NEW.achi_id = (SELECT COUNT(*) FROM Achievement)+1;
+	IF NEW.achi_id != (SELECT MAX(achi_id) FROM Achievement)+1 THEN
+		SET NEW.achi_id = (SELECT MAX(achi_id) FROM Achievement)+1;
 	END IF;
 END; |
 DELIMITER ;
@@ -17,10 +17,10 @@ CREATE TRIGGER UserTrigger
 BEFORE INSERT ON User
 FOR EACH ROW
 BEGIN
-	IF NEW.user_id != (SELECT COUNT(*) FROM User)+1 THEN
-		SET NEW.user_id = (SELECT COUNT(*) FROM User)+1;
+	IF NEW.user_id != (SELECT MAX(user_id) FROM User)+1 THEN
+		SET NEW.user_id = (SELECT MAX(user_id) FROM User)+1;
 	END IF;
-	IF NEW.location_id >= (SELECT COUNT(*) FROM Location) THEN
+	IF NEW.location_id >= (SELECT MAX(location_id) FROM Location) THEN
 		SET NEW.location_id = 0;
 	END IF;
 END; |
@@ -32,11 +32,14 @@ CREATE TRIGGER TweetTrigger
 BEFORE INSERT ON Tweet
 FOR EACH ROW
 BEGIN
-	IF NEW.tweet_id != (SELECT COUNT(*) FROM Tweet)+1 THEN
-		SET NEW.tweet_id = (SELECT COUNT(*) FROM Tweet)+1;
+	IF NEW.tweet_id != (SELECT MAX(tweet_id) FROM Tweet)+1 THEN
+		SET NEW.tweet_id = (SELECT MAX(tweet_id) FROM Tweet)+1;
 	END IF;
-	IF NEW.content.length >= 200 THEN
+	IF length(NEW.content) >= 200 THEN
 		SET NEW.content = NULL;
+	END IF;
+	IF NEW.location_id >= (SELECT MAX(location_id) FROM Location) THEN
+		SET NEW.location_id = 0;
 	END IF;
 END; |
 DELIMITER |
